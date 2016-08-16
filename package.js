635bdb39f1ab45b3a8907e90f1cc53346db525c9
Benjamin Liu <the.ben.liu@gmail.com>
2016-08-15 21:00:42 -0400

    Cleanup the package.js file

--- package.js
+++ package.js
@@ -1,19 +1,13 @@
 Package.describe({
   name: 'blizzle:simple-spreadsheet',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
   summary: 'Simple library to read and wirte spreadsheets',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  git: 'https://github.com/benliu01/simple-spreadsheet',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.4.0.1');
-  // api.use('ecmascript');
-
   api.use('huaming:js-xlsx');
   api.use('pfafman:filesaver');
   api.mainModule('simple-spreadsheet.js', 'client');
@@ -22,7 +16,6 @@ Package.onUse(function(api) {
 });
 
 Package.onTest(function(api) {
-  // api.use('ecmascript');
   api.use('tinytest');
   api.use('huaming:js-xlsx@0.8.0');
   api.use('blizzle:simple-spreadsheet');
